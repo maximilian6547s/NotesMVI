764e05413009bb76a8903a6c0971f108a2e88c31
Maximilian6547s <maximilian.s517@gmail.com>
2021-07-27 14:41:14 +0300

    Cache and Network Error Handling

--- app/src/main/java/com/maximcuker/notesmvi/business/interactors/notelist/InsertNewNote.kt
+++ app/src/main/java/com/maximcuker/notesmvi/business/interactors/notelist/InsertNewNote.kt
@@ -1,11 +1,16 @@
 package com.maximcuker.notesmvi.business.interactors.notelist
 
+import com.maximcuker.notesmvi.business.data.cache.CacheResponseHandler
 import com.maximcuker.notesmvi.business.data.cache.abstraction.NoteCacheDataSource
 import com.maximcuker.notesmvi.business.data.network.abstraction.NoteNetworkDataSource
+import com.maximcuker.notesmvi.business.data.util.safeApiCall
+import com.maximcuker.notesmvi.business.data.util.safeCacheCall
 import com.maximcuker.notesmvi.business.domain.model.Note
 import com.maximcuker.notesmvi.business.domain.model.NoteFactory
 import com.maximcuker.notesmvi.business.domain.state.*
 import com.maximcuker.notesmvi.framework.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.util.*
@@ -20,48 +25,59 @@ class InsertNewNote(
         id: String? = null,
         title: String,
         stateEvent: StateEvent
-    ): Flow<DataState<NoteListViewState>> = flow {
+    ): Flow<DataState<NoteListViewState>?> = flow {
         val newNote = noteFactory.createSingleNote(
             id = id ?: UUID.randomUUID().toString(),
             title = title,
             body = ""
         )
-        val cacheResult = noteCacheDataSource.insertNote(newNote)
-
-        var cacheResponse: DataState<NoteListViewState>? = null
-        if (cacheResult > 0) {
-            val viewState = NoteListViewState(
-                newNote = newNote
-            )
-            cacheResponse = DataState.data(
-                response = Response(
-                    message = INSERT_NOTE_SUCCESS,
-                    uiComponentType = UIComponentType.Toast(),
-                    messageType = MessageType.Success()
-                ),
-                data = viewState,
-                stateEvent = stateEvent
-            )
-        } else {
-            cacheResponse = DataState.data(
-                response = Response(
-                    message = INSERT_NOTE_FAILED,
-                    uiComponentType = UIComponentType.Toast(),
-                    messageType = MessageType.Error()
-                ),
-                data = null,
-                stateEvent = stateEvent
-            )
+        val cacheResult = safeCacheCall(IO) {
+            noteCacheDataSource.insertNote(newNote)
         }
+
+        val cacheResponse = object : CacheResponseHandler<NoteListViewState, Long>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override suspend fun handleSuccess(resultObj: Long): DataState<NoteListViewState>? {
+                return if (resultObj > 0) {
+                    val viewState = NoteListViewState(
+                        newNote = newNote
+                    )
+                    DataState.data(
+                        response = Response(
+                            message = INSERT_NOTE_SUCCESS,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Success()
+                        ),
+                        data = viewState,
+                        stateEvent = stateEvent
+                    )
+                } else {
+                    DataState.data(
+                        response = Response(
+                            message = INSERT_NOTE_FAILED,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Error()
+                        ),
+                        data = null,
+                        stateEvent = stateEvent
+                    )
+                }
+            }
+        }.getResult()
+
         emit(cacheResponse)
 
-        updateNetwork(cacheResponse.stateMessage?.response?.message, newNote)
+        updateNetwork(cacheResponse?.stateMessage?.response?.message, newNote)
 
     }
 
-    private suspend fun updateNetwork(cacheResponse: String?, newNote:Note) {
+    private suspend fun updateNetwork(cacheResponse: String?, newNote: Note) {
         if (cacheResponse.equals(INSERT_NOTE_SUCCESS)) {
-            noteNetworkDataSource.insertOrUpdateNote(newNote)
+            safeApiCall(IO) {
+                noteNetworkDataSource.insertOrUpdateNote(newNote)
+            }
         }
     }
 
