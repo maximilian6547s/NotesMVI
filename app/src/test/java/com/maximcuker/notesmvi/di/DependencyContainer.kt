23c44868caa756587bf9ebd932b3eeeab0e87e8a
Maximilian6547s <maximilian.s517@gmail.com>
2021-07-28 13:42:42 +0300

    Fake Data Sets

--- app/src/test/java/com/maximcuker/notesmvi/di/DependencyContainer.kt
+++ app/src/test/java/com/maximcuker/notesmvi/di/DependencyContainer.kt
@@ -1,9 +1,11 @@
 package com.maximcuker.notesmvi.di
 
+import com.maximcuker.notesmvi.business.data.NoteDataFactory
 import com.maximcuker.notesmvi.business.data.cache.FakeNoteCacheDataSourceImpl
 import com.maximcuker.notesmvi.business.data.cache.abstraction.NoteCacheDataSource
 import com.maximcuker.notesmvi.business.data.network.FakeNoteNetworkDataSourceImpl
 import com.maximcuker.notesmvi.business.data.network.abstraction.NoteNetworkDataSource
+import com.maximcuker.notesmvi.business.domain.model.Note
 import com.maximcuker.notesmvi.business.domain.model.NoteFactory
 import com.maximcuker.notesmvi.business.domain.util.DateUtil
 import com.maximcuker.notesmvi.util.isUnitTest
@@ -19,19 +21,27 @@ class DependencyContainer {
     lateinit var noteNetworkDataSource: NoteNetworkDataSource
     lateinit var noteCacheDataSource: NoteCacheDataSource
     lateinit var noteFactory: NoteFactory
+    lateinit var noteDataFactory: NoteDataFactory
+    private var notesData: HashMap<String, Note> = HashMap()
 
     init {
         isUnitTest = true // for Logger.kt
     }
 
     fun build() {
+        this.javaClass.classLoader?.let {classLoader ->
+            noteDataFactory = NoteDataFactory(classLoader)
+
+            //fake data set
+            notesData = noteDataFactory.produceHashMapOfNotes(noteDataFactory.produceListOfNotes())
+        }
         noteFactory = NoteFactory(dateUtil)
         noteNetworkDataSource = FakeNoteNetworkDataSourceImpl(
-            notesData = HashMap(),
+            notesData = notesData,
             deletedNotesData = HashMap()
         )
         noteCacheDataSource = FakeNoteCacheDataSourceImpl(
-            notesData = HashMap(),
+            notesData = notesData,
             dateUtil = dateUtil
         )
     }
